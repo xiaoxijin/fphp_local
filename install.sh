21bb7845cb911de64fd824dfa17b9d49634b2c34
xiaoxijin <xiaoxijin23@126.com>
2017-04-07 17:05:20 +0800

    格式转换

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-
 apk --update add openssh \
   && sed -i s/#PermitRootLogin.*/PermitRootLogin\ yes/ /etc/ssh/sshd_config \
   && sed -i s/#Port.*/Port\ 2222/ /etc/ssh/sshd_config \
   && echo "root:root" | chpasswd \
   && rm -rf /var/cache/apk/*
-
 ssh-keygen -A
-
-
-
